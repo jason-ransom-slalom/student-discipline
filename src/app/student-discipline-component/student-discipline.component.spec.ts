eb333d325b6119cacce8539255478e537795be53
Dominic <dominicretli@slalom.com>
2022-03-09 12:27:49 -0500

    end of session 1

--- src/app/student-discipline-component/student-discipline.component.spec.ts
+++ src/app/student-discipline-component/student-discipline.component.spec.ts
@@ -13,10 +13,31 @@ describe('DetentionReportComponentComponent', () => {
   let component: StudentDisciplineComponent;
   let fixture: ComponentFixture<StudentDisciplineComponent>;
 
+  const student1 = {
+    firstName: 'bob',
+    lastName: 'ross',
+    id: '111',
+    parentPhoneNumber: '555-5555',
+    priorViolations: 0
+  };
+
+  const student2 = {
+    firstName: 'cindy',
+    lastName: 'waters',
+    id: '222',
+    parentPhoneNumber: '555-5555',
+    priorViolations: 1
+  };
+
+  const studentDisciplineServiceSpy = jasmine.createSpyObj('StudentDisciplineService', ['getStudents']);
+  studentDisciplineServiceSpy.getStudents.and.returnValue(of([student1, student2]));
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-      providers: [],
+      providers: [
+        {provide: StudentDisciplineService , useValue: studentDisciplineServiceSpy}
+      ],
       declarations: [StudentDisciplineComponent]
     })
       .compileComponents();
@@ -30,5 +51,35 @@ describe('DetentionReportComponentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should setup initial form with correct form controls', ()=> {
+    const expectedFormValue = {
+      student: '',
+      violation: '',
+      punishment: ''
+    };
+
+    expect(component.form.value).toEqual(expectedFormValue);
+  });
+
+  it('should display student select element', () => {
+    const studentSelect = fixture.debugElement.query(By.css('#student'));
+    expect(studentSelect).toBeTruthy();
+  });
+
+  it('should call getStudents from the StudentDisciplineService and set the response to the components students array property', ()=> {
+    const expectedStudents = [student1, student2];
+    expect(component.students).toEqual(expectedStudents);
+  });
+
+  it('should display option for each student in the student dropdown', ()=> {
+    const studentSelect = fixture.debugElement.query(By.css('#student'));
+    const options = studentSelect.queryAll(By.css('option'));
+
+    expect(options.length).toEqual(2);
+
+    const expectedText1 = student1.firstName + ' ' + student1.lastName;
+    expect(options[0].nativeElement.textContent).toEqual(expectedText1);
   });
 });
--- src/app/student-discipline-component/student-discipline.component.ts
+++ src/app/student-discipline-component/student-discipline.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Student} from "../student";
+import {StudentDisciplineService} from "../student-discipline.service";
 
 @Component({
   selector: 'app-student-discipline',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-discipline.component.css']
 })
 export class StudentDisciplineComponent implements OnInit {
-  constructor() {
+  students: Student[];
+  form: FormGroup = this.formBuilder.group({
+    student: [''],
+    violation: [''],
+    punishment: [''],
+  })
+
+  constructor(private formBuilder: FormBuilder, private studentDisciplineService: StudentDisciplineService) {
   }
 
   ngOnInit(): void {
+    this.studentDisciplineService.getStudents().subscribe(response => {
+
+      //if response has an error
+      //displayError(response.error)
+
+      // else
+      this.students = response;
+    })
+
+    //call the student service
+    //save the response in a students propety
   }
 
 }
